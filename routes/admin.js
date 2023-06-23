d1881e549a208da0b8dcd0bae7a7823a92043d1b
phamdung2906 <eualmercury1@gmail.com>
2023-06-23 08:46:51 +0700

    a

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const admin = express.Router()
+const router = express.Router()
 
 const { uploadMiddleware } = require("../middleware/multerMiddleware");
 const amin = require("../admin");
-admin.post("/admin", uploadMiddleware.any(), async (req, res) => {
+router.post("/admin", uploadMiddleware.any(), async (req, res) => {
     const data = req.body;
 
     const infor = await amin.find();
@@ -14,4 +14,4 @@ admin.post("/admin", uploadMiddleware.any(), async (req, res) => {
     } else res.json({ status: "Failed" });
 });
 
-module.exports = admin
+module.exports = router
--- routes/hotproducts.js
+++ routes/hotproducts.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const hotproducts = express.Router()
+const router = express.Router()
 const Products = require("../Products");
-hotproducts.get("/hotproducts", async (req, res) => {
+router.get("/hotproducts", async (req, res) => {
     const hotproducts = await Products.find({ hot: "true" });
     return res.status(200).json({ status: "OK", data: hotproducts });
 });
 
 
-module.exports = hotproducts
+module.exports = router
